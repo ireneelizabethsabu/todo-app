5658c16c6542b52fd136e497d121dcebbf6a4459
ireneelizabethsabu <irenepalanatt@gmail.com>
2021-06-06 23:58:56 +0530

    Adding footer skeleton

---
+++ src/components/FooterComponent.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import { Container, Row } from 'reactstrap';
+
+class Footer extends Component {
+    render(){
+        return (
+        <Container fluid={true} className="footer">
+            <Row className="justify-content-center py-3">
+                <span><a href="/">Telegram</a></span>
+                <span><a href="/">Github</a></span>
+                <span><a href="/">Linkedin</a></span>                  
+            </Row>
+            <Row className="justify-content-center"> 
+                <p><span>&copy; 2021 by Irene Elizabeth Sabu</span></p>
+            </Row>
+        </Container>
+        )
+    }
+}
+
+export default Footer;
